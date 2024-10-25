365bfc3cc036b4a7dedd487fd907ccc1cfe0a592
DigiquestConsultancy <techteamdigiquest@gmail.com>
2024-10-25 18:34:33 +0530

    change database

--- digiquest/settings.py
+++ digiquest/settings.py
@@ -109,8 +109,13 @@ WSGI_APPLICATION = 'digiquest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digiquestazure',
+        'USER': 'Digiquestuser@digiquestserver',
+        'PASSWORD': 'Digiquest!123@45456',
+        'HOST': 'digiquestserver.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
